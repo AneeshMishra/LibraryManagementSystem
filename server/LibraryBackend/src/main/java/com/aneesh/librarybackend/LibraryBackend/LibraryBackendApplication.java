6fb005a2841f70f3a2c69a494e324426392e1e25
ANEESH-PC\ANEESH <aneesh.mishra50@gmail.com>
2019-04-28 18:08:13 +0530

    first

---
+++ server/LibraryBackend/src/main/java/com/aneesh/librarybackend/LibraryBackend/LibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aneesh.librarybackend.LibraryBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryBackendApplication.class, args);
+	}
+
+}
